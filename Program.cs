7fbf1f2bb7daa6c755545b2178e59f26f4fadf01
João Paulo <joaopdejesus79@gmail.com>
2021-09-25 17:39:37 -0300

    Programa Finalizado!

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HoraSistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime agora = DateTime.Now;
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"\nAgora é exatamente {agora.DayOfWeek}, {agora}...\n");
+            Console.ResetColor();
+        }
+    }
+}
